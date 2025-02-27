ca997ff961ff807e156b931c75e610c27a7af927
Valtteri <savonen.emppu@gmail.com>
2025-02-27 11:06:13 +0200

    added footer and bug fixed mobile

--- components/List.ts
+++ components/List.ts
@@ -1,23 +1,5 @@
 import { element } from "valzu-core/dist";
 
-function handleCurrentlyClick(e: Event) {
-  e.preventDefault();
-  const target = e.currentTarget as HTMLAnchorElement;
-  const hiddenDiv = document.getElementById("hiddenDiv") as HTMLDivElement;
-  const arrowSpan = target.querySelector("span.link-icon") as HTMLSpanElement;
-  if (hiddenDiv && arrowSpan) {
-    if (hiddenDiv.style.display === "block") {
-      hiddenDiv.style.display = "none";
-      target.classList.remove("active");
-      arrowSpan.textContent = "↗";
-    } else {
-      hiddenDiv.style.display = "block";
-      target.classList.add("active");
-      arrowSpan.textContent = "←";
-    }
-  }
-}
-
 export default function List() {
   return element(
     "div",
@@ -49,21 +31,16 @@ export default function List() {
         { href: "mailto:valtteri@bittive.com", target: "_blank" },
         "EMAIL",
         element("span", { class: "link-icon" }, "↗")
-      ),
+      )
+    ),
+    element(
+      "footer",
+      { class: "footer" },
+      element("p", {}, "Made with"),
       element(
-        "div",
-        { class: "personal-links" },
-        element(
-          "a",
-          { href: "#", class: "currently", onClick: handleCurrentlyClick },
-          "CURRENTLY",
-          element("span", { class: "link-icon" }, "↗")
-        )
-      ),
-      element(
-        "div",
-        { id: "hiddenDiv", style: "display:none;" },
-        "This is the hidden text that appears on click."
+        "a",
+        { href: "https://github.com/valtterisa/valzu.js", target: "_blank" },
+        "valzu.js"
       )
     )
   );
